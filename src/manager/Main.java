450e9710a7ec58650b06cee3092c93b4a298f872
debugger <parapet-origami.0o@icloud.com>
2023-03-06 18:30:34 +0900

    update ver 2.1
    ver 2.1

--- src/manager/Main.java
+++ src/manager/Main.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-// ver.2.0
+// ver.2.1
 public class Main {
 	public static void main(String[] args) {
 		Map<Integer, MemberVo> db = new HashMap<>();
@@ -63,5 +63,8 @@ public class Main {
 		}
 		System.out.println("전체 삭제 후 전체조회완료");
 		// git study - second commit practice
+
+		// add ver 2.1 code
+		// blah blah
 	}
 }	
